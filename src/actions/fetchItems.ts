92d3e0699a2935a8cb90b99545257f9e82faecdf
Adiar <datkaevadiar@gmail.com>
2024-06-01 23:17:23 +0500

    feat: findItemsByName

--- src/actions/fetchItems.ts
+++ src/actions/fetchItems.ts
@@ -8,3 +8,12 @@ export const fetchItems = async (point: string) => {
 		console.log(err);
 	}
 };
+
+export const fetchItemByName = async (point: string, name: string) => {
+	try {
+		const res = await axios.get(`http://localhost:4444/${point}/search?name=${name}`);
+		return res.data;
+	} catch (err) {
+		console.log(err);
+	}
+};
--- src/app/main/components/cards.tsx
+++ src/app/main/components/cards.tsx
@@ -2,33 +2,55 @@
 
 import { useEffect, useState } from 'react';
 import CardComponent from './card';
-import { fetchItems } from '@/actions/fetchItems';
+import { fetchItems, fetchItemByName } from '@/actions/fetchItems';
 
 export default function CardsComponent({ point }: { point: string }) {
 	const [items, setItems] = useState([]);
+	const [name, setName] = useState('');
+
+	const fetchData = async () => {
+		try {
+			const data = await fetchItems(point);
+			setItems(data);
+		} catch (error) {
+			console.error('Error fetching items:', error);
+		}
+	};
+
+	const handleSearch = async () => {
+		try {
+			const data = await fetchItemByName(point, name);
+			setItems(data);
+		} catch (error) {
+			console.error('Error searching items:', error);
+		}
+	};
 
 	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const data = await fetchItems(point);
-				setItems(data);
-			} catch (error) {
-				console.error('Error fetching items:', error);
-			}
-		};
-
 		fetchData();
 	}, [point]);
 
 	return (
-		<main className='grid grid-cols-4 gap-10'>
-			{items.map((item, index) => (
-				<CardComponent
-					key={index}
-					data={item}
-					type={point}
+		<div>
+			<div className='my-5'>
+				<input
+					type='text'
+					value={name}
+					onChange={e => setName(e.target.value)}
+					placeholder='Search by name'
+					className='bg-muted outline-none py-2 px-1 rounded-sm mr-2'
 				/>
-			))}
-		</main>
+				<button onClick={handleSearch}>Search</button>
+			</div>
+			<main className='grid grid-cols-4 gap-10'>
+				{items.map((item, index) => (
+					<CardComponent
+						key={index}
+						data={item}
+						type={point}
+					/>
+				))}
+			</main>
+		</div>
 	);
 }
