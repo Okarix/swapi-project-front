5c3c7d32ba5d44ebe4d4b84f33975584423aae87
Adiar <datkaevadiar@gmail.com>
2024-06-01 23:30:06 +0500

    feat: added pagination

--- src/actions/fetchItems.ts
+++ src/actions/fetchItems.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export const fetchItems = async (point: string) => {
+export const fetchItems = async (point: string, pageNumber: number = 1, pageSize: number = 10) => {
 	try {
-		const res = await axios.get(`http://localhost:4444/${point}`);
+		const res = await axios.get(`http://localhost:4444/${point}?pageNumber=${pageNumber}&pageSize=${pageSize}`);
 		return res.data;
 	} catch (err) {
 		console.log(err);
--- src/app/main/components/cards.tsx
+++ src/app/main/components/cards.tsx
@@ -7,18 +7,24 @@ import { fetchItems, fetchItemByName } from '@/actions/fetchItems';
 export default function CardsComponent({ point }: { point: string }) {
 	const [items, setItems] = useState([]);
 	const [name, setName] = useState('');
+	const [pageNumber, setPageNumber] = useState(1);
+	const [isLoading, setIsLoading] = useState(false);
 
-	const fetchData = async () => {
+	const fetchData = async (append = false) => {
 		try {
-			const data = await fetchItems(point);
-			setItems(data);
+			setIsLoading(true);
+			const data = await fetchItems(point, pageNumber);
+			setItems(prevItems => (append ? [...prevItems, ...data] : data));
+			setIsLoading(false);
 		} catch (error) {
 			console.error('Error fetching items:', error);
+			setIsLoading(false);
 		}
 	};
 
 	const handleSearch = async () => {
 		try {
+			setPageNumber(1);
 			const data = await fetchItemByName(point, name);
 			setItems(data);
 		} catch (error) {
@@ -26,9 +32,19 @@ export default function CardsComponent({ point }: { point: string }) {
 		}
 	};
 
+	const handleLoadMore = () => {
+		setPageNumber(prevPageNumber => prevPageNumber + 1);
+	};
+
 	useEffect(() => {
 		fetchData();
 	}, [point]);
+
+	useEffect(() => {
+		if (pageNumber > 1) {
+			fetchData(true);
+		}
+	}, [pageNumber]);
 
 	return (
 		<div>
@@ -38,7 +54,7 @@ export default function CardsComponent({ point }: { point: string }) {
 					value={name}
 					onChange={e => setName(e.target.value)}
 					placeholder='Search by name'
-					className='bg-muted outline-none py-2 px-1 rounded-sm mr-2'
+					className='bg-muted outline-none py-2 px-1 rounded-sm mr-2 text-[#feda4a]'
 				/>
 				<button onClick={handleSearch}>Search</button>
 			</div>
@@ -51,6 +67,15 @@ export default function CardsComponent({ point }: { point: string }) {
 					/>
 				))}
 			</main>
+			<div className='text-center mt-5'>
+				<button
+					onClick={handleLoadMore}
+					className='bg-yellow-500 text-white py-2 px-4 rounded'
+					disabled={isLoading}
+				>
+					{isLoading ? 'Loading...' : 'More'}
+				</button>
+			</div>
 		</div>
 	);
 }
